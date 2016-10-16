a903b7f8b59118d3ef49ded72400525a225b667a
Thien <thienpnguyen@yahoo.com>
2016-10-15 21:59:52 -0700

    Assignment 1 answer.

---
+++ lab1/app.js
@@ -0,0 +1,32 @@
+(function () {
+    'use strict';
+
+    angular.module('LunchCheck', [])
+           .controller('LunchCheckController', LunchCheckController);
+
+    LunchCheckController.$inject = ['$scope'];
+    function LunchCheckController($scope) {
+        $scope.lunchMenu = "1,2,3";
+        $scope.resultMsg = "";
+
+        $scope.submit = function () {
+            var numItem = 0;
+            if( $scope.lunchMenu )
+            {
+                var items = $scope.lunchMenu.split(",");
+                numItem = items.length;
+            }
+            
+            
+            if( numItem == 0)
+                $scope.resultMsg = "Please enter data first";
+            else if( numItem <= 3)
+                $scope.resultMsg = "Enjoy!";
+            else
+                $scope.resultMsg = "Too much!";
+
+        };
+
+    }
+    
+})();
